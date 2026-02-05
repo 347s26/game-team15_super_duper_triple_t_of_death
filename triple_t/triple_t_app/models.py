c04a7164892e0bc228d98e59a72f3e54082b7cc3
Brendan Walls <wallsbr@dukes.jmu.edu>
2026-02-05 14:02:25 -0500

    Database pseudocode

--- triple_t/triple_t_app/models.py
+++ triple_t/triple_t_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class State(models.Model):
+    [Win, In progress, Tie]
+
+class Piece(models.Model):
+    piece = [X, O, NULL]
+
+class Box(models.Model):
+    board = models.ForeignKey("Board", on_delete=models.PROTECT)
+    piece = models.ForeignKey("Piece", on_delete=models.PROTECT)
+
+class Board(models.Model):
+    game = models.ForeignKey("Game", on_delete=models.RESTRICT)
+
+class Game(models.Model):
+    state = models.ForeignKey("State", on_delete=models.PROTECT)
+    winner = models.ForeignKey("Piece", on_delete=models.PROTECT)
