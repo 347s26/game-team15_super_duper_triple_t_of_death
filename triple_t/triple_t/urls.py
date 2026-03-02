1378e1d4a60e267aa9f570dd71e84c496562d903
Calvin Boulier <bouliecb@dukes.jmu.edu>
2026-03-02 18:04:50 -0500

    main page template

--- triple_t/triple_t/urls.py
+++ triple_t/triple_t/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', api.urls),
     path('', include('triple_t_app.urls')),
+    path('accounts/', include('django.contrib.auth.urls')), 
 ]
